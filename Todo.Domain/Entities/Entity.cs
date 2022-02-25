174c27a11c06b9cd175622860f1ff0d33ff62420
Mateus Nadal <nadalmateus@hotmail.com>
2022-02-25 09:57:32 -0300

    feat: added todo model

---
+++ Todo.Domain/Entities/Entity.cs
@@ -0,0 +1,12 @@
+namespace Todo.Domain.Entities
+{
+    public abstract class Entity
+    {
+        public Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; private set; }
+    }
+}
